f785230cee9c814ec52297926e3f2625bc931696
Your Name <nayan.dev5572@gmail.com>
2026-08-06 16:50:59 +0600

    install and setup redux

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -69,25 +69,24 @@ export const adminRoutes = adminPaths.reduce((acc: TRoute[], item) => {
 
 
 // sidebar items for admin panel
-export const adminSidebarItems = adminPaths.reduce((acc:tSidebarItem[], item) =>{
-    if(item.path && item.name){
+
+export const adminSidebarItems = adminPaths.reduce((acc: tSidebarItem[], item) => {
+    if (item.path && item.name) {
         acc.push({
             key: item.name,
             label: <NavLink to={`/admin/${item.path}`}>{item.name}</NavLink>
         });
     }
 
-    if(item.children){
-        item.children.forEach((child) => {
-            acc.push({
+    if (item.children) {
+        acc.push({
+            key: item.name,
+            label: item.name,
+            children: item.children.map((child) => ({
                 key: child.name,
-                label: <NavLink to={`/admin/${child.path}`}>{child.name}</NavLink>,
-                children: item.children.map((childItem) => ({
-                    key: childItem.name,
-                    label: <NavLink to={`/admin/${childItem.path}`}>{childItem.name}</NavLink>
-                }))
-            });
-        })
+                label: <NavLink to={`/admin/${child.path}`}>{child.name}</NavLink>
+            }))
+        });
     }
     return acc;
 }, []);
