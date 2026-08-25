8265ac3b231016671403e98a4ffe29a8ea0203dc
Your Name <nayan.dev5572@gmail.com>
2026-08-25 23:52:24 +0600

    create store

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,36 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
 
 
 const Login = () => {
+
+    const {register, handleSubmit} = useForm();
+
+    const [login, {data, isLoading, isError}] = useLoginMutation();
+    
+
+    const onSubmit = (data: any) => {
+        const userInfo = {
+            id: data.id,
+            password: data.password
+        };
+        login(userInfo);
+    }
+
     return (
-        <div>
-            <h1>Login page</h1>
-        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div>
+                <label htmlFor="id">ID</label>
+                <input type="text" id="id" {...register("id")} />
+            </div>
+            <div>
+                <label htmlFor="id">Password</label>
+                <input type="password" id="password" {...register("password")} />
+            </div>
+            <Button type="primary" htmlType="submit">Login</Button>
+        </form>
     );
 };
 
